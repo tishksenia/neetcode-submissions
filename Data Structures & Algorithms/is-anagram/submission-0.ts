ee460b92f797e22d7b1e6b227bbafce0d1a2c351
Ksenia Tischenko <kseniia_tishchenko@fastmail.com>
2026-05-23 17:58:36 +0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,45 @@
+type CharMap = Map<string, number>;
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const sMap = this.buildMap(s);
+        const tMap = this.buildMap(t);
+
+        console.log(sMap, tMap);
+
+        return this.areMapsEqual(sMap, tMap);
+    }
+
+    areMapsEqual(a: CharMap, b: CharMap): boolean {
+        if(a.size != b.size) {
+            return false;
+        }
+
+        for(let [key, value] of a) {
+            if(b.get(key) !== value) {
+                return false;
+            }
+        }
+        return true
+    }
+
+    buildMap(w: string): CharMap {
+        const chars = w.split('');
+        const map = new Map();
+        for(let c of chars) {
+            if(map.has(c)) {
+                map.set(c, map.get(c) + 1);
+            }
+            else {
+                map.set(c, 1);
+            }
+        }
+
+        return map;
+    }
+}
